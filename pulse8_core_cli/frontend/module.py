8a69ce58f5f094f5347a78b826781b42015943e3
Chamil Udayanga <chamil.udayanga@synpulse.com>
2023-10-19 14:39:28 +0800

    [P8C-574] Added nextjs frontend integration

--- pulse8_core_cli/frontend/module.py
+++ pulse8_core_cli/frontend/module.py
@@ -1,5 +1,7 @@
 import typer
-
+import os
+from typing_extensions import Annotated
+from copier import run_copy, run_update
 
 app = typer.Typer()
 
@@ -10,13 +12,27 @@ def dev():
 
 
 @app.command()
-def create():
-    print(f"[WIP]...")
+def create(
+    github_username: Annotated[str, typer.Option(
+        prompt=True, confirmation_prompt=False, help="Synpulse github username")]
+):
+
+    print("\n")
+    print('Pulling latest template data...')
+
+    run_copy(
+        f"https://{github_username}@github.com/synpulse-group/pulse8-core-frontend-nextjs-template.git", ".")
+
+    os.system("git init")
+    os.system("git add .")
+    os.system(
+        "git commit -m \"[PULSE8] Generated using Pulse8 Core Template\" ")
 
 
 @app.command()
 def update():
-    print(f"[WIP]...")
+    print('Pulling latest template data...')
+    run_update('.', overwrite=True)
 
 
 @app.command()
