2afe7d3d0d602642a9aae218c85e0e17dba75e2d
DennisMac <dennis.weller@profect.de>
2023-10-30 16:18:25 +0100

    updated create and update to align with other templates

---
+++ pulse8_core_cli/frontend_angular/functions.py
@@ -0,0 +1,52 @@
+import os
+
+import typer
+from copier import run_copy, run_update
+from rich import print
+
+from pulse8_core_cli.environment.functions import ENV_GITHUB_USER, ENV_GITHUB_TOKEN
+from pulse8_core_cli.shared.module import git_init, get_env_variables, REPOSITORY_PRIVATE, REPOSITORY_INTERNAL, \
+    git_create_remote
+
+
+def frontend_angular_create(create_remote_repo: str, answers_file: str, defaults: bool, skip_answered: bool,
+                            folder_name: str):
+    if not folder_name:
+        folder_name = typer.prompt("Enter a folder name")
+
+    env_variables = get_env_variables()
+    github_user = env_variables[ENV_GITHUB_USER]
+    github_token = env_variables[ENV_GITHUB_TOKEN]
+
+    if create_remote_repo is None:
+        create_remote_input = typer.prompt(
+            "Do you want to create private or internal repository ? [no/private/internal]")
+
+        if create_remote_input == REPOSITORY_PRIVATE or create_remote_input == REPOSITORY_INTERNAL:
+            create_remote_repo = create_remote_input
+    elif create_remote_repo != REPOSITORY_PRIVATE and create_remote_repo != REPOSITORY_INTERNAL:
+        create_remote_repo = None
+
+    print("Pulling latest template data...")
+
+    worker = run_copy(
+        f"https://{github_user}:{github_token}@github.com/synpulse-group/pulse8-core-backend-template.git",
+        folder_name, unsafe=True, defaults=defaults, answers_file=answers_file, skip_answered=skip_answered)
+
+    project_id = worker.answers.user.get("project_id")
+
+    os.chdir(folder_name)
+    os.system("pnpm install")
+    os.system("husky install")
+
+    git_init()
+    git_create_remote(create_remote_repo, project_id, github_user, github_token)
+
+
+def frontend_angular_update(answers_file: str, defaults: bool, skip_answered: bool):
+    print("Pulling latest template data...")
+
+    run_update(".", overwrite=True, unsafe=True, defaults=defaults,
+               answers_file=answers_file, skip_answered=skip_answered)
+
+    print("[green]Project successfully updated.[/green]")
--- pulse8_core_cli/frontend_angular/module.py
+++ pulse8_core_cli/frontend_angular/module.py
@@ -1,7 +1,7 @@
-import os
-
 import typer
-from copier import run_copy, run_update
+from typing_extensions import Annotated
+
+from pulse8_core_cli.frontend_angular.functions import frontend_angular_create, frontend_angular_update
 
 app = typer.Typer()
 
@@ -12,36 +12,23 @@ def dev():
 
 
 @app.command()
-def create(
-        folder_name: str = typer.Argument(None, help="Folder name for your project.")
-):
-    if not folder_name:
-        folder_name = typer.prompt("Enter a folder name")
-
-    print("\n")
-    print('Pulling latest template data...')
-
-    run_copy(
-        f"git@github.com:synpulse-group/pulse8-core-frontend-angular-template.git", folder_name)
-
-    os.chdir(folder_name)
-    os.system("pnpm install")
-    os.system("husky install")
-    os.system("git init")
-    os.system("git add .")
-    os.system(
-        "git commit -m \"[PULSE8] Generated using Pulse8 Core Template\" ")
+def create(answers_file: Annotated[str, typer.Option(help="Copier answers file path")] = None,
+           create_remote_repository: Annotated[str,
+           typer.Option(help="Create remote repository [options: no/private/internal]")] = None,
+           defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
+           skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = False,
+           folder_name: str = typer.Argument(None, help="Folder name for your project.")
+           ):
+    frontend_angular_create(create_remote_repository, answers_file, defaults, skip_answered, folder_name)
 
 
 @app.command()
 def update(
-        use_saved_answers: bool = typer.Argument(True, help="Decide if the old answers are used")
+        answers_file: Annotated[str, typer.Option(help="Copier answers file path")] = None,
+        defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
+        skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = True
 ):
-    use_saved_answers = typer.confirm("Do you want to use the saved answers?", default=True)
-
-    print('Checking template tags')
-    run_update('.', overwrite=True, defaults=use_saved_answers)
-    print('Update completed.')
+    frontend_angular_update(answers_file, defaults, skip_answered)
 
 
 @app.command()
