7ed24f4bc42f1620e10541fef75aae2b7f65c72b
jakub.hoferik <jakub.hoferik@synpulse.com>
2023-11-27 14:38:07 +0100

    added cli integration to create and update fastapi template

--- pulse8_core_cli/backend_fastapi/module.py
+++ pulse8_core_cli/backend_fastapi/module.py
@@ -1,5 +1,8 @@
+from typing import Annotated
+
 import typer
 
+from pulse8_core_cli.backend_fastapi.functions import backend_fastapi_update, backend_fastapi_create
 
 app = typer.Typer()
 
@@ -13,19 +16,25 @@ def dev():
 
 
 @app.command()
-def create():
+def create(answers_file: Annotated[str, typer.Option(help="Copier answers file path")] = None,
+           create_remote_repository: Annotated[str,
+           typer.Option(help="Create remote repository [options: no/private/internal]")] = None,
+           defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
+           skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = False):
     """
     Create a new backend
     """
-    print(f"[WIP]...")
+    backend_fastapi_create(create_remote_repository, answers_file, defaults, skip_answered)
 
 
 @app.command()
-def update():
+def update(answers_file: Annotated[str, typer.Option(help="Copier answers file path")] = None,
+           defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
+           skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = True):
     """
     Update an existing backend
     """
-    print(f"[WIP]...")
+    backend_fastapi_update(answers_file, defaults, skip_answered)
 
 
 @app.command()
