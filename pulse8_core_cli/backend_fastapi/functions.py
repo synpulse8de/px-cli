957bb3a208046eb34a975d12f7430ae9970c8458
Jakub Hoferik <jakub.hoferik@synpulse.com>
2023-11-28 13:42:26 +0100

    added folder creation based on "project_id" input from copier questions, adjusted all templates to use same reusable methods to create and update template if its ok so we have aligned aproach - all creation was similar, tested all commands for all templates, frontend nextjs didnt create folder so now it creates and also optionally creates repo and contains all optional arguments like other templates, angular template now creates folder also based on "project_id" instead of additional question in cli "folder_name", refactored code for better structure

--- pulse8_core_cli/backend_fastapi/functions.py
+++ pulse8_core_cli/backend_fastapi/functions.py
@@ -1,44 +1,10 @@
-import typer
-
-from copier import run_copy, run_update
-from rich import print
-
-from pulse8_core_cli.environment.functions import ENV_GITHUB_USER, ENV_GITHUB_TOKEN
-from pulse8_core_cli.shared.module import git_init, get_env_variables, REPOSITORY_PRIVATE, REPOSITORY_INTERNAL, \
-    git_create_remote
+from pulse8_core_cli.shared.constants import TEMPLATE_REPO_BACKEND_FASTAPI
+from pulse8_core_cli.shared.template_management import create_template, update_template
 
 
 def backend_fastapi_create(create_remote_repo: str, answers_file: str, defaults: bool, skip_answered: bool):
-
-    env_variables = get_env_variables()
-    github_user = env_variables[ENV_GITHUB_USER]
-    github_token = env_variables[ENV_GITHUB_TOKEN]
-
-    if create_remote_repo is None:
-        create_remote_input = typer.prompt(
-            "Do you want to create private or internal repository ? [no/private/internal]")
-
-        if create_remote_input == REPOSITORY_PRIVATE or create_remote_input == REPOSITORY_INTERNAL:
-            create_remote_repo = create_remote_input
-    elif create_remote_repo != REPOSITORY_PRIVATE and create_remote_repo != REPOSITORY_INTERNAL:
-        create_remote_repo = None
-
-    print("Pulling latest template data...")
-
-    worker = run_copy(
-        f"https://{github_user}:{github_token}@github.com/synpulse-group/pulse8-core-backend-fastapi-template.git",
-        ".", unsafe=True, defaults=defaults, answers_file=answers_file, skip_answered=skip_answered)
-
-    project_id = worker.answers.user.get("project_id")
-
-    git_init()
-    git_create_remote(create_remote_repo, project_id, github_user, github_token)
+    create_template(TEMPLATE_REPO_BACKEND_FASTAPI, create_remote_repo, answers_file, defaults, skip_answered)
 
 
 def backend_fastapi_update(answers_file: str, defaults: bool, skip_answered: bool):
-    print("Pulling latest template data...")
-
-    run_update(".", overwrite=True, unsafe=True, defaults=defaults,
-               answers_file=answers_file, skip_answered=skip_answered)
-
-    print("[green]Project successfully updated.[/green]")
+    update_template(answers_file, defaults, skip_answered)
--- pulse8_core_cli/frontend_angular/module.py
+++ pulse8_core_cli/frontend_angular/module.py
@@ -19,21 +19,17 @@ def create(answers_file: Annotated[str, typer.Option(help="Copier answers file p
            create_remote_repository: Annotated[str,
            typer.Option(help="Create remote repository [options: no/private/internal]")] = None,
            defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
-           skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = False,
-           folder_name: str = typer.Argument(None, help="Folder name for your project.")
-           ):
+           skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = False):
     """
     Create a new frontend
     """
-    frontend_angular_create(create_remote_repository, answers_file, defaults, skip_answered, folder_name)
+    frontend_angular_create(create_remote_repository, answers_file, defaults, skip_answered)
 
 
 @app.command()
-def update(
-        answers_file: Annotated[str, typer.Option(help="Copier answers file path")] = None,
-        defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
-        skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = True
-):
+def update(answers_file: Annotated[str, typer.Option(help="Copier answers file path")] = None,
+           defaults: Annotated[bool, typer.Option(help="Use default answers and skip questions")] = False,
+           skip_answered: Annotated[bool, typer.Option(help="Skip answered questions")] = True):
     """
     Update an existing frontend
     """
