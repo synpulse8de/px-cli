89b01927fe5e9dae950b5c797bd87fdbd14371f1
Jakub Hoferik <jakub.hoferik@synpulse.com>
2024-01-18 16:01:19 +0100

    fixed issue of git with creation of template in elevated command prompt

--- pulse8_core_cli/shared/module.py
+++ pulse8_core_cli/shared/module.py
@@ -53,6 +53,8 @@ def git_init():
     os.system('git add .')
     os.system('git commit -m "[PULSE8] Generated using Pulse8 Core Template" --quiet')
     os.system('git branch -M main')
+    current_git_path = os.getcwd().replace('\\', '/')
+    os.system(f'git config --global --add safe.directory {current_git_path}')
 
 
 def git_create_remote(create_remote_repo: str, repository_name: str, github_user: str, github_token: str):
